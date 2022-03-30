4734f72c30176763073ac360ecc383008cee0e44
Sorcerer <sorcerer@jd6.so-net.ne.jp>
2022-03-30 13:12:47 +0900

    Add files via upload

---
+++ 00_variables.tf
@@ -0,0 +1,39 @@
+variable "pj_name" {
+  type = map
+  default = {
+    name   = "sorcererspiral"
+  }
+}
+
+variable "common-AZ" {
+  type = map
+  default = {
+    region = "ap-northeast-1"
+    az-1   = "ap-northeast-1a"
+    az-2   = "ap-northeast-1c"
+  }
+}
+
+variable "vpc" {
+  type = map
+  default = {
+    cidr-block = "10.0.0.0/24"
+  }
+}
+
+variable "rds-dbname-1" {
+  type = map
+  default = {
+    dbname   = "wpdb"
+  }
+}
+
+variable "rds-user-1" {
+  type = map
+  default = {
+    user   = "wpuser"
+  }
+}
+
+data "aws_caller_identity" "self" { }
+data "aws_region" "current" {}
